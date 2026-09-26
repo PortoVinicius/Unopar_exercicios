c3fef7cb53dfb1d00afb6a6d66d54d35bf242eae
Vinicius <141542477+PortoVinicius@users.noreply.github.com>
2026-09-26 02:26:01 +0000

    scores1

--- algoritmos_programacao_estruturada/scores.c
+++ algoritmos_programacao_estruturada/scores.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-    // Scores
+    // Get scores Usando loop
     int scores[3];
-    scores[0] = 72;
-    scores[1] = 73;
-    scores[2] = 33;
+
+    for (int i = 0; i < 3; i++) {
+        printf("Score: ");
+        scanf("%d", &scores[i]);
+    }
 
     // Print average
     printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
