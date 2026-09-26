1d6e1e5f5f504dd648426bd3528c7a390652024b
Vinicius <141542477+PortoVinicius@users.noreply.github.com>
2026-09-26 00:44:16 +0000

    boog usando função print

--- algoritmos_programacao_estruturada/array.c
+++ algoritmos_programacao_estruturada/array.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
+void print_column(int height);
+
 int main(void) {
     int n;
     printf("Higth: ");
     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("#");
-        }
-        printf("\n");
-    }
-    
+    print_column(n);
     return 0;
 }
+
+void print_column(int height) {
+
+    for (int i = 0; i < height; i++) {
+        printf("%d\n", i);
+    }
+}
+    
