ccc252b897e9c1769306d7619cc0104232dc6dc7
Vinicius <141542477+PortoVinicius@users.noreply.github.com>
2026-09-26 01:17:58 +0000

    add scores

---
+++ algoritmos_programacao_estruturada/scores.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    // Scores
+    int score1 = 72;
+    int score2 = 73;
+    int score3 = 33;
+
+    printf("score1 = %d\nscore2 = %d\nscore3 = %d\n", score1, score2, score3);
+} 
